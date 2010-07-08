8771c9f88b7def2a38e03159fd05947ae8741f36
gb.viel <gb.viel@19dc031e-e4bf-11de-91d6-89caad0352f8>
2010-07-08 12:58:14 +0000

    Suite de ELF dans proc.c
    git-svn-id: https://rhinos.googlecode.com/svn/trunk@479 19dc031e-e4bf-11de-91d6-89caad0352f8

--- kern/proc.h
+++ kern/proc.h
@@ -56,6 +56,10 @@
 #define PROC_IDLE_DATA_SIZE     1
 #define PROC_IDLE_TICKETS       1
 
+/* Flags pour certains ELF */
+
+#define ELF_MM                  1
+
 /***************
  * Structures
  ***************/
@@ -133,6 +137,6 @@ PUBLIC void sched_init(struct skip_list* list);
 PUBLIC void task_index(u32_t* index);
 PUBLIC void task_init(struct proc* pr, u32_t index, u32_t code_base, u32_t code_size, u32_t data_base, u32_t data_size, u32_t stack_base, u32_t stack_size, u8_t priv, u32_t entry_point, u32_t tickets);
 PUBLIC void task_schedule();
-PUBLIC void task_elf(u32_t* addr);
+PUBLIC void task_elf(u32_t* addr, u8_t flags);
 
 #endif
