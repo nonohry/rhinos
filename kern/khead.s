378cdf8aefb9d1a75b88982f09e134862ce590c3
gb.viel <gb.viel@19dc031e-e4bf-11de-91d6-89caad0352f8>
2010-08-12 13:24:41 +0000

    Remaniement du script de link
    git-svn-id: https://rhinos.googlecode.com/svn/trunk@524 19dc031e-e4bf-11de-91d6-89caad0352f8

--- kern/khead.s
+++ kern/khead.s
@@ -10,7 +10,8 @@ extern	irq_active		; Tableau des ISR actives en C
 extern	excep_handle		; Handlers pour les exceptions en C
 extern	tss			; TSS defini en C
 extern	proc_current		; Pointeur sur le processus a executer
-extern  main			; RhinOS Main en C	
+extern  main			; RhinOS Main en C
+extern	kernel_start		; Debut du Noyau (issu de l edition des liens)	
 extern	kernel_end		; Fin du Noyau (issu de l edition des liens)
 	
 global _start			; Point d'entree pour le lien
@@ -53,7 +54,7 @@ global	excep_18
 global	task_mgmt		; Gestion des taches
 
 _start:
-	mov	dword [ecx], KLOADOFF 		; Champs kern_start de boot_info
+	mov	dword [ecx], kernel_start 	; Champs kern_start de boot_info
 	mov	dword [ecx+4], kernel_end 	; Champs kern_end de boot_info
 	push	ecx		; Empile l adresse de boot_info
 	push	pmodemsg	; Empile le message
@@ -340,12 +341,6 @@ excep_err_next:
 	pmodemsg 	db	'Protected Mode enabled !',13,10,0
 	excep_code	dd	0 ; Code Erreur des exceptions
 	excep_num	dd	0 ; Vecteur de l exception
-
-	;;
-	;; Offset du Noyau
-	;; 
-
-	KLOADOFF	equ	0xC00
 	
 	;; 
 	;; Segment Selector
