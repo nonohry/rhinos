2770691a66e66d26c1701a3d693c37f400cba7cc
gb.viel <gb.viel@19dc031e-e4bf-11de-91d6-89caad0352f8>
2010-08-17 12:18:10 +0000

    Support de %d et %x dans bochs_print (ATTENTION: pas securise car juste pour le debuggage)
    git-svn-id: https://rhinos.googlecode.com/svn/trunk@548 19dc031e-e4bf-11de-91d6-89caad0352f8

--- kern/klib.h
+++ kern/klib.h
@@ -3,7 +3,7 @@
 
 #include <types.h>
 
-EXTERN void bochs_print(char*);
+EXTERN void bochs_print(char*,...);
 EXTERN void outb(u16_t,u8_t);
 EXTERN void inb(u16_t,u8_t*);
 EXTERN void phys_copy(u32_t, u32_t, u32_t);
--- kern/klib.s
+++ kern/klib.s
@@ -8,23 +8,80 @@ global random
 global idle_task
 	
 	;; 
-	;; Affichage dans bochs
-	;; 
+	;; Affichage dans bochs (%d et %x supporte)
+	;;
+
+ASCII_OFFSET0	equ	48
+ASCII_OFFSETA	equ	55	
+BASE		dw	0
 	
 bochs_print:
 	push 	ebp         	; Sauvegarde de EBP
 	mov  	ebp,esp 	; Mise en place de la base
 	push	esi		; Sauvegarde ESI (Requis par GCC)
 	push	edi		; Sauvegarde EDI (Requis par GCC)
-	mov  	esi,[ebp+8]	; Recupere l'argument dans ESI
+	push	edx		; Sauvegarde EDX
+	mov	edx,ebp		; Copie EBP
+	add	edx,8		; Place EDX sur le premier argument
+	mov  	esi,[edx]	; Recupere l'argument dans ESI
 bochs_loop:	
 	lodsb			; Identique a print_message
 	cmp	al,0		; Fin de chaine ?
 	je	bochs_end	; On retourne si oui
+	cmp	al,'%'		; Caractere '%' ?
+	je	bochs_percent	; On s'en occupe si oui
+	push	edx		; Sauvegarde EDX
 	mov	dx,0xe9		; Port de Bochs
 	out	dx,al		; Emet le caractere courant
+	pop	edx		; Restaure EDX
 	jmp	bochs_loop	; Boucle
+bochs_percent:
+	lodsb			; Avale le caractere suivant
+bochs_base10:
+	cmp	al,'d'		; Regarde si c est un 'd'
+	jne	bochs_base16	; Si non, on saute
+	mov	[BASE],dword 10 ; Base decimale
+	jmp	bochs_next	; Saute a la decomposition
+bochs_base16:
+	cmp	al,'x'		; Regarde si c est un 'd'
+	jne	bochs_base16	; Si non, on saute
+	mov	[BASE],dword 16 ; Base hexadecimale
+bochs_next:	
+	push	eax	 	; Sauvegarde EAX
+	push	ebx		; Sauvegarde EBX
+	push	ecx		; Sauvegarde ECX
+	add	edx,4		; EDX pointe sur l argument suivant
+	mov	eax,[edx]	; Met la valeur de l argument dans EAX
+	mov	ecx,0		; Initialise le compteur de division
+	mov	ebx,dword [BASE]; Initialise le diviseur
+	push	edx		; Sauvegarde EDX
+bochs_decomp:
+	mov	edx,0		; Zero EDX (car on divise EDX-EAX)
+	div	ebx		; Divise par la base
+	add	ecx,1		; Increment le compteur de division
+	push	edx		; Empile le Reste
+	cmp	eax,0		; Fin de la division ?
+	jnz	bochs_decomp	; Si non, on continue la decomposition
+bochs_depop:
+	pop	eax		; Depile dans AL
+	cmp	eax,9		; Compare la valeur a 9
+	jg	bochs_ascii16	; Change l'offset pour les nombres hexa
+	add	eax,ASCII_OFFSET0 ; Ajoute l offset pour avoir le code ASCII (0->9)
+	jmp	bochs_ascii_next  ; Saute a l affichage
+bochs_ascii16:
+	add	eax,ASCII_OFFSETA ; Ajoute l offset pour avoir le code ASCII (A->F)
+bochs_ascii_next:	
+	mov	dx,0xe9		; Port de Bochs
+	out	dx,al		; Emet le caractere courant
+	sub	ecx,1		; Decremente le compteur
+	jnz	bochs_depop	; Boucle tant que le compteur est non nul
+	pop	edx		; Restaure EDX
+	pop	ecx		; Restaure ECX
+	pop	ebx		; Restaure EBX
+	pop	eax		; Restaure EAX
+	jmp	bochs_loop	; Boucle sur le reste de la chaine	
 bochs_end:
+	pop	edx		; Restaure EDX
 	pop	edi		; Restaure EDI
 	pop	esi		; Restaure ESI
 	mov	esp,ebp		; Restaure la pile
