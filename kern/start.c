fb4769125d7ca39beaaf6526d7458e14618d8784
gb.viel <gb.viel@19dc031e-e4bf-11de-91d6-89caad0352f8>
2011-01-02 19:34:19 +0000

    Relecture du memory map en C ---- CA MERDE !
    git-svn-id: https://rhinos.googlecode.com/svn/trunk@562 19dc031e-e4bf-11de-91d6-89caad0352f8

--- kern/start.c
+++ kern/start.c
@@ -21,8 +21,20 @@
 
 PUBLIC void cstart(struct boot_info* binfo)
 { 
+  u32_t i;
+  struct boot_memmap_entry* entry;
+
   /* Recopie les informations de demarrage */
   bootinfo = *binfo;
+
+  /* Recupere le memory map s'il existe */
+  entry = (struct boot_memmap_entry*)bootinfo.mem_addr;
+
+  for(i=0;i<bootinfo.mem_entry;i++)
+    {
+      bochs_print("%x%x | %x%x | %x\n",entry->base_addr_up, entry->base_addr_low, entry->size_up, entry->size_low, entry->type);
+      entry ++;
+    }
 
   /* Initialise le mode protege (gdt, interruptions ...) */
   pmode_init();
--- kern/start.h
+++ kern/start.h
@@ -17,10 +17,23 @@ PUBLIC struct boot_info
   u32_t kern_start;     /* Debut du noyau */
   u32_t kern_end;       /* Fin du noyau */
   u32_t mem_addr;       /* Adresse du memory map */
-  u16_t  mem_entry;     /* Nombre d entree dans le memory map */
+  u16_t mem_entry;      /* Nombre d entree dans le memory map */
   u16_t mem_lower;      /* Memoire basse en Ko */
   u16_t mem_upper;      /* Memoire haute en 64Ko */
 };
+
+/******************************
+ * Structure boot_memmap_entry
+ ******************************/
+
+PUBLIC struct boot_memmap_entry
+{
+  u32_t base_addr_low;  /* Addresse de la zone - 32 premiers bits */
+  u32_t base_addr_up;   /* Addresse de la zone - 32 derniers bits */
+  u32_t size_low;       /* Taille de la zone - 32 permiers bits */
+  u32_t size_up;        /* Taille de la zone - 32 permiers bits */
+  u32_t type;           /* Type de memoire */
+} __attribute__ ((packed));
 
 
 /*************
