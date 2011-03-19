c45302158bfb02da37da34255425980df6eafd5d
gb.viel <gb.viel@19dc031e-e4bf-11de-91d6-89caad0352f8>
2011-03-19 20:09:43 +0000

    Petuite modif llist
    git-svn-id: https://rhinos.googlecode.com/svn/trunk@587 19dc031e-e4bf-11de-91d6-89caad0352f8

--- include/llist.h
+++ include/llist.h
@@ -10,53 +10,67 @@
 
 
 /* Initialisation de la liste */
-#define LLIST_INIT(e) \
+#define LLIST_NULLIFY(e)			\
   (e) = (void*)0;
 
-/* Initialisee ? */
-#define LLIST_ISINIT(e) \
+
+/* NULL ? */
+#define LLIST_ISNULL(e)				\
   ( (e)==(void*)0 )
 
-/* Tete de la liste */
-#define LLIST_HEAD(e) \
-{ \
-  (e)->next = e;				\
-  (e)->prev = e;				\
-}
 
-/* Tete de liste ? */
-#define LLIST_ISHEAD(e) \
-  ( ((e)->next == e) && ((e)->prev == e) )	\
+/* Tete de la liste */
+#define LLIST_HEAD(e)				\
+  {						\
+    (e)->next = e;				\
+    (e)->prev = e;				\
+  }
+
+/* Liste singleton */
+#define LLIST_ISSINGLE(l)			\
+  ( ((l)->next == l)&&((l)->prev == l)		
+
+/* Comparaison a la tete */
+#define LLIST_ISHEAD(l,e)			\
+  {						\
+    ( (e)==(l) );				\
+  }
 
 /* Ajout a la liste */
-#define LLIST_ADD(l,e) \
-{ \
-  (e)->next = (l)->next; \
-  (e)->prev = (l); \
-  ((l)->next)->prev = (e); \
-  (l)->next = (e); \
+#define LLIST_ADD(l,e)				\
+  {						\
+    (e)->next = (l)->next;			\
+    (e)->prev = (l);				\
+    ((l)->next)->prev = (e);			\
+    (l)->next = (e);				\
 }
 
 /* Suppression */
-#define LLIST_REMOVE(l,e) \
-{ \
- ((e)->prev)->next = (e)->next;  \
- ((e)->next)->prev = (e)->prev; \
-  if ((l)==(e)) \
-  { \
-   (l)=(l)->next; \
-  } \
-}
+#define LLIST_REMOVE(l,e)			\
+  {						\
+    ((e)->prev)->next = (e)->next;		\
+    ((e)->next)->prev = (e)->prev;		\
+    if LLIST_ISHEAD(l,e)			\
+    {						\
+      if LLIST_ISSINGLE(l)			\
+	{					\
+	  LLIST_NULLIFY(l);			\
+	}					\
+      else					\
+	{					\
+	  (l)=(l)->next;			\
+	}					\
+    }						\
+  }
 
 
 /* Next */
-#define LLIST_NEXT(l,e) \
-	(e)->next;
-	
-/* Previous */
-#define LLIST_PREV(l,e) \
-	(e)->prev;
+#define LLIST_NEXT(l,e)				\
+  (e)->next;
 
+/* Previous */
+#define LLIST_PREV(l,e)				\
+  (e)->prev;
 
 
 #endif
