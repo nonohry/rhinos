f92c6a11c09c3984e78b9fac4d0bbe4d37472635
gb.viel <gb.viel@19dc031e-e4bf-11de-91d6-89caad0352f8>
2011-06-17 09:54:04 +0000

    ajout du watermark allocator sous forme de macros
    git-svn-id: https://rhinos.googlecode.com/svn/trunk@744 19dc031e-e4bf-11de-91d6-89caad0352f8

---
+++ include/wmalloc.h
@@ -0,0 +1,25 @@
+/*
+ *
+ * Macros Water Mark Allocator
+ *
+ */
+
+#ifndef WMALLOC_H
+#define WMALLOC_H
+
+/* Initialisation */
+#define WMALLOC_INIT(wm,wmbase,wmsize)		\
+  {						\
+    (wm).base = wmbase;				\
+    (wm).size = wmsize;				\
+    (wm).offset = 0;				\
+  }
+
+/* Allocation */
+#define WMALLOC_ALLOC(wm,wmsize)			\
+  (void*)( (wm).offset < (wm).size ? ( ((wm).offset+=wmsize) ? ( (wm).offset <= (wm).size ? ((wm).base+(wm).offset) : 0 ) : 0 ) : 0 ); 
+
+/* Liberation */
+#define WMALLOC_FREE(wm,addr)			
+
+#endif
