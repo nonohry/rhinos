9f039c67296e7dcdaff4c3c190de9682cb5c8597
gb.viel <gb.viel@19dc031e-e4bf-11de-91d6-89caad0352f8>
2011-07-15 08:25:35 +0000

    Structures du slab allocator
    git-svn-id: https://rhinos.googlecode.com/svn/trunk@776 19dc031e-e4bf-11de-91d6-89caad0352f8

--- kern/virtmem_slab.h
+++ kern/virtmem_slab.h
@@ -8,5 +8,60 @@
 #define VIRTMEM_SLAB_H
 
 
+/************
+ * Includes
+ ***********/
+
+#include <types.h>
+
+
+/**************
+ * Constantes
+ **************/
+
+#define VIRT_CACHE_NAMELEN     32
+
+
+/**************
+ * Structures
+ **************/
+
+
+/* Bufctl */
+struct vmem_bufctl
+{
+  virtaddr_t base;
+  struct vmem_slab* slab;
+  struct vmem_bufctl* next;
+  struct vmem_bufctl* prev;
+};
+
+
+/* Slab */
+struct vmem_slab
+{
+  u32_t count;
+  struct vmem_bufctl* free_buf;
+  struct vmem_cache* cache;
+  struct vmem_slab* next;
+  struct vmem_slab* prev;
+};
+
+
+/* Cache */
+struct vmem_cache
+{
+  char name[VIRT_CACHE_NAMELEN];
+  u32_t size;
+  u32_t align;
+  void (*constructor)(void*,u32_t);
+  void (*destructor)(void*,u32_t);
+  struct vmem_slab* slabs_free;
+  struct vmem_slab* slabs_partial;
+  struct vmem_slab* slabs_full;
+  struct vmem_cache* next;
+  struct vmem_cache* prev;
+};
+
 
 #endif
