9aec0668f765f515221192e859fd2dfff431ea29
G4b <gb.viel@gmail.com>
2012-10-11 10:13:28 +0200

    Introduction de la notion de processus (Un espace d 'adressage et une liste nd ethreads pour le moment)

---
+++ kern/proc.c
@@ -0,0 +1,20 @@
+/*
+ * Gestion des processus
+ *
+ */
+
+
+
+/*========================================================================
+ * Includes
+ *========================================================================*/
+
+
+#include <types.h>
+#include <llist.h>
+#include "const.h"
+#include "thread.h"
+#include "klib.h"
+#include "virtmem_slab.h"
+#include "paging.h"
+#include "proc.h"
---
+++ kern/proc.h
@@ -0,0 +1,52 @@
+/*
+ * proc.h
+ * Header de proc.c
+ *
+ */
+
+
+#ifndef PROC_H
+#define PROC_H
+
+
+/*========================================================================
+ * Includes
+ *========================================================================*/
+
+
+#include <types.h>
+#include "const.h"
+#include "thread.h"
+
+
+/*========================================================================
+ * Structures
+ *========================================================================*/
+
+
+/* Helper pour le chainage des threads */
+
+PUBLIC struct thread_info
+{
+  struct thread* thread;
+  struct thread_info* prev;
+  struct thread_info* next;
+};
+
+
+/* Structure proc */
+
+PUBLIC struct proc
+{
+  virtaddr_t v_pd;
+  physaddr_t p_pd;
+  struct thread_info* thread_list;
+};
+
+
+/*========================================================================
+ * Prototypes
+ *========================================================================*/
+
+
+#endif
