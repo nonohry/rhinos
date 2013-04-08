3c1ecd0d4809f6a5891405126fe726a5994b0556
G4b <gb.viel@gmail.com>
2013-04-08 15:07:21 +0200

    doc: paging.h

--- kern/paging.h
+++ kern/paging.h
@@ -1,22 +1,36 @@
-/*
- * Header de la pagination
- *
- */
+/**
+
+   paging.h
+   ========
+
+   Pagination header. 
+   Define useful constants and macros
+
+**/
 
 #ifndef PAGING_H
 #define PAGING_H
 
-/*========================================================================
- * Includes
- *========================================================================*/
+/**
+
+   Includes
+   --------
+
+   - types.h
+   - const.h
+
+**/
 
 #include <types.h>
 #include "const.h"
 
 
-/*========================================================================
- * Constantes
- *========================================================================*/
+/**
+
+   Constants: paging relatives
+   ---------------------------
+
+**/
 
 #define PAGING_ENTRIES      1024
 #define PAGING_DIRSHIFT     22
@@ -32,29 +46,65 @@
 #define PAGING_SELFMAP      0x3FF
 
 
-/*========================================================================
- * Structures
- *========================================================================*/
+/**
 
-/* Page directory entry */
+   Structure: struct pde
+   ---------------------
+
+   Describe an 32 bits entry in page directory.
+   Members are:
+
+     - present   : page in memory or not
+     - rw        : read/write access 
+     - user      : user/supervisor right
+     - pwt       : write through
+     - pcd       : cache disabled
+     - accessed  : page accessed
+     - zero      : nil
+     - pagesize  : page size
+     - global    : global page
+     - available : available bits
+     - baseaddr  : page table physical address
+
+**/
 
 PUBLIC struct pde
 {
-  u32_t present   :1  ;  /* Page presente en memoire  */
-  u32_t rw        :1  ;  /* Page en lecture/ecriture  */
-  u32_t user      :1  ;  /* Privilege User/Supervisor */
-  u32_t pwt       :1  ;  /* Write through             */
-  u32_t pcd       :1  ;  /* Cache disable             */
-  u32_t accessed  :1  ;  /* Page accedee (ro ou rw)   */ 
-  u32_t zero      :1  ;  /* Zero                      */
-  u32_t pagesize  :1  ;  /* Taille des pages          */
-  u32_t global    :1  ;  /* Page globale              */
-  u32_t available :3  ;  /* Bits disponibles          */ 
-  u32_t baseaddr  :20 ;  /* Adresse physique          */
+  u32_t present   :1  ;
+  u32_t rw        :1  ;
+  u32_t user      :1  ;
+  u32_t pwt       :1  ;
+  u32_t pcd       :1  ;
+  u32_t accessed  :1  ;
+  u32_t zero      :1  ;
+  u32_t pagesize  :1  ;
+  u32_t global    :1  ;
+  u32_t available :3  ;
+  u32_t baseaddr  :20 ;
 }__attribute__ ((packed));
 
 
-/* Page table entry */
+/**
+
+   Structure: struct pte
+   ---------------------
+
+   Describe an 32 bits entry in page table.
+   Members are:
+
+     - present   : page in memory or not
+     - rw        : read/write access 
+     - user      : user/supervisor right
+     - pwt       : write through
+     - pcd       : cache disabled
+     - accessed  : page accessed
+     - dirty     : page was written
+     - zero      : nil
+     - global    : global page
+     - available : available bits
+     - baseaddr  : physical page address
+
+**/
 
 PUBLIC struct pte
 {
@@ -64,7 +114,7 @@ PUBLIC struct pte
   u32_t pwt       :1  ;
   u32_t pcd       :1  ;
   u32_t accessed  :1  ;
-  u32_t dirty     :1  ;  /* Page ecrite */
+  u32_t dirty     :1  ;
   u32_t zero      :1  ;
   u32_t global    :1  ;
   u32_t available :3  ;
@@ -72,41 +122,107 @@ PUBLIC struct pte
 }__attribute__ ((packed));
 
 
-/*========================================================================
- * Macros
- *========================================================================*/
+/**
 
-/* Directory d'une adresse virtuelle */
+   Macro: PAGING_GET_PDE(__addr)
+   -----------------------------
+
+   Extract the page directory entry number from a virtual address
+
+**/
+
 #define PAGING_GET_PDE(__addr)				\
   ( (__addr) >> PAGING_DIRSHIFT )
 
-/* Table d'une adresse virtuelle */
+
+
+/**
+
+   Macro: PAGING_GET_PTE(__addr)
+   -----------------------------
+   
+   Extract the page table entry number from a virtual address
+
+**/
+
 #define PAGING_GET_PTE(__addr)				\
   ( ((__addr) >> PAGING_TBLSHIFT)&PAGING_TBLMASK )
 
-/* Alignement inferieur sur CONST_PAGE_SIZE */
+
+/**
+
+   Macro: PAGING_ALIGN_INF(__addr)
+   -------------------------------
+
+   Align `__addr` on  lower page size boundary
+
+**/
+
 #define PAGING_ALIGN_INF(__addr)			\
   ( ((__addr) >> CONST_PAGE_SHIFT) << CONST_PAGE_SHIFT )
 
-/* Alignement superieur sur CONST_PAGE_SIZE */
+
+/**
+
+   Macro: PAGING_ALIGN_INF(__addr)
+   -------------------------------
+
+   Align `__addr` on  upper page size boundary
+
+**/
+
 #define PAGING_ALIGN_SUP(__addr)						\
   ( (((__addr)&0xFFFFF000) == (__addr))?((__addr) >> CONST_PAGE_SHIFT) << CONST_PAGE_SHIFT:(((__addr) >> CONST_PAGE_SHIFT)+1) << CONST_PAGE_SHIFT )
 
-/* Self Mapping: Page Directory courant */
+
+/**
+
+   Macro: PAGING_GET_PD()
+   ----------------------
+
+   Return virtual address of current page directory thanks to self maping
+
+**/
+ 
 #define PAGING_GET_PD()							\
   ( (virtaddr_t)(PAGING_SELFMAP<<PAGING_DIRSHIFT) + (virtaddr_t)(PAGING_SELFMAP<<PAGING_TBLSHIFT) )
 
-/* Self Mapping: Page Table i du PD courant */
+
+/**
+
+   Macro: PAGING_GET_PT(__i)
+   -------------------------
+
+   Return virtual address of page table pointed by the entry `i` 
+   in current page directory thanks to self maping
+
+**/
+
 #define PAGING_GET_PT(__i)						\
   ( (virtaddr_t)(PAGING_SELFMAP<<PAGING_DIRSHIFT) + (virtaddr_t)((__i)<<PAGING_TBLSHIFT) )
 
 
-/*========================================================================
- * Prototypes
- *========================================================================*/
 
-/* PD Noyau */
+/**
+
+   Global: struct pde* kern_PD
+   ---------------------------
+
+   Kernel Page Directory
+
+**/
+
 PUBLIC struct pde* kern_PD;
+
+
+/**
+   
+   Prototypes
+   ----------
+
+   Give access to mapping and unmapping as well as initialization and conversion utility
+
+**/
 
 PUBLIC u8_t paging_init(void);
 PUBLIC u8_t paging_map(virtaddr_t vaddr, physaddr_t paddr, u8_t flags);
