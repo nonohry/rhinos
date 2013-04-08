c539b0eec61ed6fed42ef0df15b3de60e4d55eb3
G4b <gb.viel@gmail.com>
2013-04-08 16:37:23 +0200

    doc: physmem.h

--- kern/physmem.h
+++ kern/physmem.h
@@ -1,15 +1,28 @@
-/*
- * Header de phymem.c
- *
- */
+/**
+
+   physmem.h
+   =========
+
+   Header of physical memory manager
+   Define useful constants and macros 
+
+**/
 
 #ifndef PHYSMEM_C
 #define PHYSMEM_C
 
 
-/*========================================================================
- * Includes 
- *========================================================================*/
+/**
+
+   Includes
+   --------
+
+   - types.h
+   - const.h
+   - virtmem_slab.h  : struct vmem_cache &  struct vmem_bufctl needed
+   - virtmem_buddy.h : struct vmem_area needed
+
+**/   
 
 #include <types.h>
 #include "const.h"
@@ -17,9 +30,13 @@
 #include "virtmem_buddy.h"
 
 
-/*========================================================================
- * Constantes 
- *========================================================================*/
+/**
+
+   Constants: Physical memory relatives
+   ------------------------------------
+
+
+**/
 
 #define PHYS_PAGE_MAX_BUDDY      21        /* 32 - 12 + 1 = 21 */
 #define PHYS_POOL_AREA_START     CONST_PAGE_NODE_POOL_ADDR
@@ -32,29 +49,74 @@
 #define PHYS_UNMAP_NOFREE     1
 
 
-/*========================================================================
- * Macros
- *========================================================================*/
+/**
+
+   Macro: PHYS_ALIGN_INF(__addr)
+   -----------------------------
+
+   Align `__addr` on  lower page size boundary
+
+**/
 
 
-/* Alignement inferieur sur PPAGE_SIZE */
-#define PHYS_ALIGN_INF(__addr)			\
+#define PHYS_ALIGN_INF(__addr)					\
   ( ((__addr) >> CONST_PAGE_SHIFT) << CONST_PAGE_SHIFT )
 
-/* Alignement superieur sur PPAGE_SIZE */
+
+/**
+
+   Macro: PHYS_ALIGN_SUP(__addr)
+   -----------------------------
+
+   Align `__addr` on upper page size boundary
+
+**/
+
 #define PHYS_ALIGN_SUP(__addr)						\
   ( (((__addr)&0xFFFFF000) == (__addr))?((__addr) >> CONST_PAGE_SHIFT) << CONST_PAGE_SHIFT:(((__addr) >> CONST_PAGE_SHIFT)+1) << CONST_PAGE_SHIFT )
 
-/* Description de page d une adresse donnee */
+
+/**
+
+   Macro: PHYS_GET_DESC(__addr)
+   ----------------------------
+
+   Get the page descriptor bounded to `__addr`. 
+   Every physical page address has its own page descriptor 
+   located at `PHYS_POOL_AREA_START + ((__addr) >> CONST_PAGE_SHIFT)*sizeof(struct ppage_desc)`
+
+**/
+
 #define PHYS_GET_DESC(__addr)						\
   ( (struct ppage_desc*)(PHYS_POOL_AREA_START + ((__addr) >> CONST_PAGE_SHIFT)*sizeof(struct ppage_desc)) )
 
-/* Existence d une page description */
+
+/**
+
+   Macro: PHYS_PDESC_ISNULL(__pdescaddr)
+   -------------------------------------
+
+   Tell if the page descriptor is null. 
+   Null is defined here as equal to PHYS_POOL_AREA_START.
+
+**/
+
+
 #define PHYS_PDESC_ISNULL(__pdescaddr)		\
   ( ((physaddr_t)(__pdescaddr)) == PHYS_POOL_AREA_START )
 
 
-/* Zeroing d un ppage_desc */
+
+/**
+
+   Macro: PHYS_NULLIFY_DESC(__desc)
+   --------------------------------
+
+   Nullify all the fields of the page descriptor `__desc`
+
+**/
+   
+
 #define PHYS_NULLIFY_DESC(__desc)			\
   {							\
     (__desc)->start = 0;				\
@@ -69,11 +131,27 @@
   }
 
 
-/*========================================================================
- * Structures 
- *========================================================================*/
+/**
+   Structure: struct ppage_desc 
+   ----------------------------
 
-/* Noeud des buddies */
+   Contains the description of a physical memory area and is used as item in the buddy system.
+   Each CONST_PAGE_SIZE page ownes a page descriptor but a page descriptor can represent several
+   contiguous pages area.
+   All the page descriptors are linked in a double linked list.
+   Members are:
+   
+   - start   : area base address
+   - size    : area size in bytes
+   - maps    : number of virtual mappings
+   - index   : level in the buddy system
+   - cache   : list of struct vmem_cache in the page
+   - bufctl  : list of struct vmem_bufctl in the page
+   - area    : list of struct vmem_area in the page
+   - prev    : previous item in the linked list
+   - next    : next item in the linked list
+
+**/
 
 PUBLIC struct ppage_desc 
 {
@@ -89,9 +167,14 @@ PUBLIC struct ppage_desc
 }__attribute__((packed));
 
 
-/*========================================================================
- * Prototypes 
- *========================================================================*/
+/**
+
+   Prototypes
+   ----------
+
+   Give access to intialization, allocation & freeing and mapping/unmapping operations.
+
+**/
 
 PUBLIC u8_t phys_init(void);
 PUBLIC void* phys_alloc(u32_t size);
