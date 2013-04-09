9490dfecf794d63327b00de49bb5c6a45bb1009e
G4b <gb.viel@gmail.com>
2013-04-09 17:00:22 +0200

    doc: syscall.h

--- kern/syscall.h
+++ kern/syscall.h
@@ -1,32 +1,64 @@
+/**
+
+   syscall.h
+   =========
+
+   System calls header
+
+**/
+
 #ifndef SYSCALL_H
 #define SYSCALL_H
 
 
-/*========================================================================
- * Includes 
- *========================================================================*/
+/**
+
+   Includes 
+   --------
+
+   - types.h
+   - const.h
+
+**/
 
 #include <types.h>
 #include "const.h"
 
 
-/*========================================================================
- * Constantes
- *========================================================================*/
+/**
+  
+   Constants: Syscall numbers
+   --------------------------
+   
+**/
 
 
 #define SYSCALL_SEND        1
 #define SYSCALL_RECEIVE     2
 #define SYSCALL_NOTIFY      3
 
+
+/**
+  
+   Constants: Syscall IPC states
+   -----------------------------
+   
+**/
+
+
 #define SYSCALL_IPC_SENDING    1
 #define SYSCALL_IPC_RECEIVING  2
 #define SYSCALL_IPC_NOTIFYING  3
 
 
-/*========================================================================
- * Prototypes 
- *========================================================================*/
+/**
+
+   Prototypes
+   ----------
+
+   Give access to the syscall handler
+
+**/
 
 PUBLIC void syscall_handle();
 
