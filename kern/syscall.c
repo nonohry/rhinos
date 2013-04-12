2430dda91c2bf9028f32c0d6ea649c8688999f00
G4b <gb.viel@gmail.com>
2013-04-12 22:21:15 +0200

    doc: syscall.c (partial)

--- kern/syscall.c
+++ kern/syscall.c
@@ -1,13 +1,34 @@
-/*
- * Syscall
- * Traitement des appels systemes
- *
- */
+/**
+   
+   syscall.c
+   =========
+
+   Kernel syscalls.
+   Provide classical microkernel IPC API (send & receive) 
+
+**/
 
 
-/*========================================================================
- * Includes 
- *========================================================================*/
+/**
+
+   Includes 
+   ========
+
+
+   - types.h
+   - llist.h
+   - ipc.h           : IPC constnats needed
+   - const.h
+   - klib.h
+   - thread.h        : struct thread needed
+   - sched.h         : scheduler queue manipulation
+   - physmem.h       : phys_alloc needed
+   - virtmem_buddy.h : virtual buddy allocation/release 
+   - paging.h        : mapping physical/virtual
+   - syscall.h       : self header
+
+
+**/
 
 #include <types.h>
 #include <llist.h>
@@ -22,9 +43,14 @@
 #include "syscall.h"
 
 
-/*========================================================================
- * Declaration PRIVATE
- *========================================================================*/
+/**
+
+   Privates
+   --------
+
+   Effective IPC primitives
+
+**/
 
 
 PRIVATE u8_t syscall_send(struct thread* th_sender, struct thread* th_receiver, struct ipc_message* message);
@@ -33,12 +59,21 @@ PRIVATE u8_t syscall_notify(struct thread* th_from, struct thread* th_to);
 PRIVATE u8_t syscall_copymsg(struct ipc_message* message, physaddr_t phys_msg, u8_t op);
 
 
-/*========================================================================
- * Point d entree
- *========================================================================*/
+/**
+
+   Function: void syscall_handle(void)
+   -----------------------------------
+
+   Syscall main handler
+   Redispatch calls to the correct IPC primitive according to syscall number 
+   retrieved in the caller CPU context.
+
+   Result is returned to caller through its EAX register
+
+**/
 
 
-PUBLIC void syscall_handle()
+PUBLIC void syscall_handle(void)
 {
   struct thread* cur_th;
   struct thread* target_th;
@@ -47,7 +82,7 @@ PUBLIC void syscall_handle()
   s32_t arg_id;
   u8_t res;
 
-  /* Le thread courant */
+  /* Get current thread */
   cur_th = sched_get_running_thread();
   if (cur_th == NULL)
     {
@@ -55,27 +90,28 @@ PUBLIC void syscall_handle()
       goto end;
     }
 
-  /* Le numero d appel dans les registres */
+  /* Get syscall number in EDX */
   syscall_num = (u32_t)(cur_th->cpu.edx);
 
-  /* Les arguments dans les registres */
+  /* Get destination thread (to send to or to receive from) in EBX */
   arg_id = (s32_t)(cur_th->cpu.ebx);
 
+  /* Send & receive syscall need a message. Get it in ECX */
   if (syscall_num != SYSCALL_NOTIFY)
     {
       arg_message = (struct ipc_message*)(cur_th->cpu.ecx);
-       /* Indique le createur du message */
+      /* Set message originator */
       arg_message->from = cur_th->id->id;
     }
 
-  /* Le thread cible */
+  /* Destination thread */
   if (arg_id == IPC_ANY)
     {
       target_th = NULL;
     }
   else
     {
-      /* Recherche le thread via son id_info */
+      /* Get thread structure from given id */
       target_th = thread_id2thread(arg_id);
       if ( target_th == NULL )
 	{
@@ -84,7 +120,7 @@ PUBLIC void syscall_handle()
 	}
     }
 
-  /* Redirection vers les fonction effectives */
+  /* Dispatch call to effective primitives */
   switch(syscall_num)
     {
     case SYSCALL_SEND:
@@ -112,41 +148,52 @@ PUBLIC void syscall_handle()
     }
 
  end:
-  /* Ecrit le retour pour le thread appelant */
+  /* Set result in caller's EAX register */
   cur_th->cpu.eax=res;
 
   return;
 }
 
 
-/*========================================================================
- * Send
- *========================================================================*/
+
+/**
+
+   u8_t syscall_send(struct thread* th_sender, struct thread* th_receiver, struct ipc_message* message)
+   ----------------------------------------------------------------------------------------------------
+
+
+   Pass ̀message` from `th_sender` to `th_receiver`.
+   First it checks for a deadlock situation (`th_receiver` sending to `th_sender`)
+   Then, if `th_receiver` is waiting for the message, ̀message` is copied from `th_sender` to `th_receiver`,
+   `th_receiver` is set as ready for scheduling and `th_sender` is set as blocked (waiting for a notify call).
+   If `th_receiver` is not waiting for any message, `th_sender` is set as blocked in `th_receiver` waiting list.
+
+**/
 
 PRIVATE u8_t syscall_send(struct thread* th_sender, struct thread* th_receiver, struct ipc_message* message)
 {
   struct thread* th_tmp;
 
-  /* Pas de broadcast */
+  /* There must be a receiver */
   if ( th_receiver == NULL )
     {
       return IPC_FAILURE;
     }
 
-  /* Indique a qui envoyer */
+  /* Set receiver in sender structure */
   th_sender->ipc.send_to = th_receiver;
 
-  /* Precise l envoi */
+  /* Set state */
   th_sender->ipc.state |= SYSCALL_IPC_SENDING;
 
-  /* Si le destinataire envoie, alors on s'assure qu on ne boucle pas */
+  /* Check for a deadlock: run through sending chain from receiver */
   if (th_receiver->ipc.state & SYSCALL_IPC_SENDING)
     {
 
       th_tmp = th_receiver->ipc.send_to;
       while( th_tmp->ipc.state & SYSCALL_IPC_SENDING )
 	{
-	  /* Deadlock */
+	  /* One thread in the sending chain is the sender: deadlock */
 	  if (th_tmp == th_sender)
 	    {
 	      return IPC_DEADLOCK;
@@ -156,36 +203,35 @@ PRIVATE u8_t syscall_send(struct thread* th_sender, struct thread* th_receiver, 
 	}
     }
 
-  /* Pas de deadlock, le message est recupere (dans l espace d adressage de l emetteur)  */
- 
+  /* No deadlock here, get message from sender address space */ 
   th_sender->ipc.send_message = message;
   th_sender->ipc.send_phys_message = paging_virt2phys((virtaddr_t)message);
 
-
-  /* Pas de message mappe en memoire physique, on retourne */
+  /* No physical mapping for message: error */
   if ( !th_sender->ipc.send_phys_message )
     {
       return IPC_FAILURE;
     }
  
-  /* Regarde si le destinataire receptionne uniquement et de la part de l emetteur */
+  /* Check if receiver is only receiving, and is receiving from the sender or any thread */
   if ( (th_receiver->ipc.state == SYSCALL_IPC_RECEIVING)
        && ( (th_receiver->ipc.receive_from == th_sender)||(th_receiver->ipc.receive_from == NULL) ) )
     {
-      /* Le destinataire est en attente de message: copie du message dans l espace destinataire */
+      /* Receiver is willing to receive: copy message from sender to receiver */
       if (syscall_copymsg(message,th_receiver->ipc.receive_phys_message,SYSCALL_SEND) != EXIT_SUCCESS)
 	{
 	  return IPC_FAILURE;
 	}
       
-      /* Debloque le destinataire au besoin */
+      /* Set receiver ready for scheduling */ 
       if (th_receiver->state == THREAD_BLOCKED)
 	{
 
 	  th_receiver->next_state = THREAD_READY;
-	  /* Fin de reception pour le destinataire */
+	  /* Set end of receiving */
 	  th_receiver->ipc.state &= ~SYSCALL_IPC_RECEIVING;
 
+	  /* Scheduler queues manipulations */
 	  if ( sched_dequeue(SCHED_BLOCKED_QUEUE, th_receiver) != EXIT_SUCCESS )
 	    {
 	      return IPC_FAILURE;
@@ -198,19 +244,19 @@ PRIVATE u8_t syscall_send(struct thread* th_sender, struct thread* th_receiver, 
 
 	}
 
-      /* Fin d envoi */
+      /* Message is delivered to receiver, set end of sending */
       th_sender->ipc.state &= ~SYSCALL_IPC_SENDING;
-      /* Bloque en attendant le traitement du message */
+      /* Sender is blocked, waiting for message processing (must be unblocked via notify) */
       th_sender->next_state = THREAD_BLOCKED;
 
     }
   else
     {
-      /* Le destinataire n'attend pas de message: bloque l emetteur dans la wait list du destinataire */
+      /* Receiver is not waiting for sender at the moment: block sender in receiver's waiting list */
       th_sender->next_state = THREAD_BLOCKED_SENDING;
     }
 
-  /* Ordonnance */
+  /* In any cases, current thread (sender) is blocked, so scheduling is needing */
   sched_schedule(SCHED_FROM_SEND);
 
   return IPC_SUCCESS;
