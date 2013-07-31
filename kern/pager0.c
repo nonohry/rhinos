6775ec9199608fe65ac4348fc0955ba3252f961c
G4b <gb.viel@gmail.com>
2013-07-31 16:01:53 +0200

    pager0

---
+++ kern/pager0.c
@@ -0,0 +1,28 @@
+/**
+
+   pager0.c
+   ========
+
+   Kernel page fault handler 
+
+**/
+
+
+
+/**
+
+   Includes
+   --------
+
+   - define.h
+   - types.h
+   - pager0.h   : self header
+   
+**/
+
+
+#include <define.h>
+#include <types.h>
+#include "pager0.h"
+
+
---
+++ kern/pager0.h
@@ -0,0 +1,31 @@
+/**
+
+   pager0.h
+   ========
+
+   Kernel page fault handler header
+
+**/
+
+
+#ifndef PAGER0_H
+#define PAGER0_H
+
+
+/**
+
+   Includes
+   --------
+
+   - define.h
+   - types.h
+   
+**/
+
+#include <define.h>
+#include <types.h>
+
+
+
+
+#endif
