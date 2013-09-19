ffe8297b2a05d0ab06b2574ae7f13cde60c74710
G4b <gb.viel@gmail.com>
2013-09-19 10:53:45 +0200

    starting tests for message apssing in ring 3

--- Makefile
+++ Makefile
@@ -14,7 +14,7 @@ CFLAGS	:=	-Iinclude -Iinclude/arch/x86
 
 
 # Objects
-OBJ_USER = srv/user.o
+OBJ_USER = srv/user_send.o srv/user_rcv.o
 OBJ_KERN = kern/arch/$(ARCH)/krt.o  kern/arch/$(ARCH)/serial.o  kern/arch/$(ARCH)/x86_lib.o kern/arch/$(ARCH)/vm_segment.o kern/arch/$(ARCH)/vm_paging.o kern/arch/$(ARCH)/setup.o kern/arch/$(ARCH)/e820.o kern/arch/$(ARCH)/context.o kern/arch/$(ARCH)/int.o kern/arch/$(ARCH)/pic.o kern/arch/$(ARCH)/exceptions.o  kern/arch/$(ARCH)/pit.o kern/arch/$(ARCH)/interrupt.o kern/main.o kern/pager0.o kern/vm_pool.o kern/vm_slab.o kern/thread.o kern/proc.o kern/sched.o kern/syscall.o kern/irq.o kern/clock.o
 #OBJ_IPC  = lib/ipc/ipc.o
 
---
+++ srv/user_send.c
@@ -0,0 +1,67 @@
+/**
+
+   user.c
+   ======
+
+   Test program that do a sendrec to a computing thread
+
+**/
+
+
+#include <define.h>
+#include <types.h>
+#include <ipc.h>
+
+void mem_copy(addr_t src, addr_t dst, u32_t len);
+
+struct calc_msg
+{
+  u8_t op_code;
+  u16_t op_1;
+  u16_t op_2;
+  u32_t op_res;
+} __attribute__((packed));
+
+
+
+
+int main()
+{
+  int j;
+  //struct ipc_message m;
+  struct calc_msg cm;
+
+  cm.op_code = 2;
+  j=1;
+
+  while(j)
+    {
+      cm.op_1 = j%10;
+      cm.op_2 = j%100;
+      /* mem_copy((addr_t)&cm,(addr_t)m.data,sizeof(struct calc_msg)); */
+      /* if (ipc_sendrec(3,&m)!=IPC_SUCCESS) */
+      /* 	{ */
+      /* 	  break; */
+      /* 	} */
+      //mem_copy((addr_t)m.data,(addr_t)&cm,sizeof(struct calc_msg));
+      //j++;
+    }
+  while(1){}
+  return 0;
+}
+
+
+
+
+void mem_copy(addr_t src, addr_t dst, u32_t len)
+{
+  char* s = (char*)src;
+  char* d = (char*)dst;
+
+  while(len--)
+    {
+      *d++ = *s++;
+    }
+
+  return;      
+}
